95dd72bf9594fab5fd441eeca0c8f6b17e05f560
Dominic Minischetti III <domminischetti@gmail.com>
2017-07-20 22:35:01 -0700

    Temporary contain all logic and view in App, Create mysteryHero method, Add Pharah

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,48 @@
 import React from 'react'
-import HeroPage from './HeroPage'
+// import HeroPage from './HeroPage'
 import HeroList from './HeroList'
 import heroes from './heroes.json'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedHero: "",
+            heroLine: ""
+        };
+        this.mysteryHero = this.mysteryHero.bind(this);
+    }
+
+    mysteryHero = () => {
+        const randomHero = Math.floor(Math.random() * heroes.roster.length);
+        const chosenHero = heroes.roster[randomHero]
+        const randomHeroLine = chosenHero.line[Math.floor(Math.random() * chosenHero.line.length)]
+        this.setState({
+            selectedHero: chosenHero,
+            heroLine: randomHeroLine
+        })
+    }
+
     render() {
         return (
-            <HeroPage/>
+            <div>
+                <span onClick={this.mysteryHero}>Mystery Hero</span>
+                <span>{this.state.selectedHero.name}</span>
+                <br/>
+                   <span>{this.state.heroLine}</span>   
+            </div>
+            // <HeroPage selectedHero = {this.mysteryHero}/>
+        )
+    }
+}
+
+class HeroPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <span onClick={this.mysteryHero}>Mystery Hero</span>
+                <span>{this.props.selectedHero}</span>
+            </div>
         )
     }
 }
