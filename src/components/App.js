3a491e6af1be44ee169431c25e199853f9f42803
Dominic Minischetti III <domminischetti@gmail.com>
2017-07-22 00:28:13 -0700

    Add assets, Add CSS stylesheet, Add support for hero background image and color, Add basic styling for mystery button, Refactor

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import heroes from './heroes.json'
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { selectedHero: "", heroLine: "" };
+        this.state = { selectedHero: "", heroLine: ""};
         this.updateHeroState = this.updateHeroState.bind(this);
     }
 
@@ -27,19 +27,25 @@ class HeroPage extends React.Component {
         this.mysteryHero = this.mysteryHero.bind(this);
     }
 
-    mysteryHero() {
-        const selectedHero = heroes.roster[Math.floor(Math.random() * heroes.roster.length)]
-        const heroLine = selectedHero.line[Math.floor(Math.random() * selectedHero.line.length)]
-        this.props.mysteryHero(selectedHero, heroLine);
+
+    selectRandom(element) {
+        return element[Math.floor(Math.random() * element.length)]
     }
 
+    mysteryHero() {
+        const selectedHero = this.selectRandom(heroes.roster)
+        const heroLine = this.selectRandom(selectedHero.line)
+        this.props.mysteryHero(selectedHero, heroLine);
+    }
     render() {
         return (
             <div>
-                 <span onClick={this.mysteryHero}>Mystery Hero</span> 
-                  <span>{this.props.selectedHero.name}</span>
+                <span className="mysteryHeroButton" onClick={this.mysteryHero}>Mystery<span className="mysteryKey">M</span></span> 
+                <span>{this.props.selectedHero.name}</span>
                 <br/>
-                 <span>{this.props.heroLine}</span> 
+                <span>{this.props.heroLine}</span>
+                <div className="backgroundTint" style={{backgroundColor: this.props.selectedHero.color}}></div>
+                <img className="heroImage" src={this.props.selectedHero.image}/>
             </div>
         )
     }
