244b6bee204f5e1644204f84c95fa150fce8e106
Dominic Minischetti III <domminischetti@gmail.com>
2017-07-21 23:34:00 -0700

    Restructure and refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,45 @@
 import React from 'react'
-// import HeroPage from './HeroPage'
-import HeroList from './HeroList'
 import heroes from './heroes.json'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedHero: "",
-            heroLine: ""
-        };
-        this.mysteryHero = this.mysteryHero.bind(this);
+        this.state = { selectedHero: "", heroLine: "" };
+        this.updateHeroState = this.updateHeroState.bind(this);
     }
 
-    mysteryHero = () => {
-        const randomHero = Math.floor(Math.random() * heroes.roster.length);
-        const chosenHero = heroes.roster[randomHero]
-        const randomHeroLine = chosenHero.line[Math.floor(Math.random() * chosenHero.line.length)]
-        this.setState({
-            selectedHero: chosenHero,
-            heroLine: randomHeroLine
-        })
+    updateHeroState(selectedHero, heroLine) {
+        this.setState({selectedHero, heroLine})
     }
 
     render() {
+        const selectedHero = this.state.selectedHero;
+        const heroLine = this.state.heroLine;
         return (
-            <div>
-                <span onClick={this.mysteryHero}>Mystery Hero</span>
-                <span>{this.state.selectedHero.name}</span>
-                <br/>
-                   <span>{this.state.heroLine}</span>   
-            </div>
-            // <HeroPage selectedHero = {this.mysteryHero}/>
+            <HeroPage mysteryHero={this.updateHeroState} selectedHero={selectedHero} heroLine={heroLine}/>
         )
     }
 }
 
 class HeroPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.mysteryHero = this.mysteryHero.bind(this);
+    }
+
+    mysteryHero() {
+        const selectedHero = heroes.roster[Math.floor(Math.random() * heroes.roster.length)]
+        const heroLine = selectedHero.line[Math.floor(Math.random() * selectedHero.line.length)]
+        this.props.mysteryHero(selectedHero, heroLine);
+    }
+
     render() {
         return (
             <div>
-                <span onClick={this.mysteryHero}>Mystery Hero</span>
-                <span>{this.props.selectedHero}</span>
+                 <span onClick={this.mysteryHero}>Mystery Hero</span> 
+                  <span>{this.props.selectedHero.name}</span>
+                <br/>
+                 <span>{this.props.heroLine}</span> 
             </div>
         )
     }
